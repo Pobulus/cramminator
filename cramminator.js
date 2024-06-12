cad5eab2e123b41b73d3db5e41fcbf71760d45db
Paweł Chmielewski <pawek.chmielewski@gmail.com>
2024-06-12 19:46:23 +0200

    review failed function

--- cramminator.js
+++ cramminator.js
@@ -1,4 +1,5 @@
 var questions = [];
+var failedQuestions =[];
 var images = [];
 var testIndex = 0;
 var files;
@@ -210,14 +211,16 @@ function checkTest() {
     }`
   );
   $("#explanationContent").text(question.explanation || "");
-  if (hardcore && !allOK) {
-    // one mistake resets the test
-    retries += 1;
-    $("#retries-counter").text(`retries: ${retries}`);
-    testIndex = -1;
-
-    player?.seekTo(0);
-    nextQuestion();
+  if(!allOK){
+    failedQuestions.push(question);
+    if (hardcore) {
+      // one mistake resets the test
+      retries += 1;
+      $("#retries-counter").text(`retries: ${retries}`);
+      testIndex = -1;
+      player?.seekTo(0);
+      nextQuestion();
+    }
   }
 }
 f.onchange = function () {
@@ -278,37 +281,42 @@ function stopVideo() {
   player.stopVideo();
 }
 function initiateCramminator() {
-  // load saved questions
-  questions = JSON.parse(localStorage.getItem("savedQuestions"));
-  images = JSON.parse(localStorage.getItem("savedImages")) || [];
-  folderName = localStorage.getItem("savedFolderName");
-  console.log("loaded saved file: ", questions);
-  if (questions) {
-    startTest();
-    $("#loadedName").text("saved file");
-  }
-  // add keyboard support
-  document.addEventListener('keydown', function(event) {
-    if(event.key == 'ArrowLeft') {
-      if(questions) prevQuestion();
+    // load saved questions
+    questions = JSON.parse(localStorage.getItem("savedQuestions"));
+    images = JSON.parse(localStorage.getItem("savedImages")) || [];
+    folderName = localStorage.getItem("savedFolderName");
+    console.log("loaded saved file: ", questions);
+    if (questions) {
+      startTest();
+      $("#loadedName").text("saved file");
     }
-    else if(event.key == 'ArrowRight') {
-      if(questions) nextQuestion();
-    }
-    else if(event.key == ' ') {
-      if(questions) checkTest();
-    }
-    else {
-      const number = Number(event.key);
-      if(number != NaN){
-        const answers  = $('td.ans');
-        let index = number!==0 ? number-1 : 9;
-        index += event.getModifierState('CapsLock')*10;
-        console.log(index);
-        answers?.[`${index}`]?.click();
+    // add keyboard support
+    document.addEventListener('keydown', function(event) {
+      if(event.key == 'ArrowLeft') {
+        if(questions) prevQuestion();
       }
-    }
-});
+      else if(event.key == 'ArrowRight') {
+        if(questions) nextQuestion();
+      }
+      else if(event.key == ' ') {
+        if(questions) checkTest();
+      }
+      else {
+        const number = Number(event.key);
+        if(number != NaN){
+          const answers  = $('td.ans');
+          let index = number!==0 ? number-1 : 9;
+          index += event.getModifierState('CapsLock')*10;
+          console.log(index);
+          answers?.[`${index}`]?.click();
+        }
+      }
+  });
+}
+function reviewFailed() {
+  questions = failedQuestions;
+  failedQuestions = [];
+  startTest();
 }
 
 window.onload = initiateCramminator;
